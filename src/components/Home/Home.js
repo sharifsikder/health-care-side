17ca011979186da8f652f23db341ed17c1378ebc
John Doe <johndoe@example.com>
2021-10-19 19:31:31 +0600

    added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
-import Contact from '../Contact/Contact';
 import Services from '../Services/Services';
-
-
 
 const Home = () => {
 
-   
     return (
         <div>
-       
-      
+  
           <Banner></Banner>
           <Services></Services>
           <About></About>
-          <Contact></Contact>
-         
+       
         </div>
     );
 };
